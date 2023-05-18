f20da12cfba319294d69343b05b4238d37eacd18
aniketbumbc <aniket.umbc@gmail.com>
2023-05-18 08:01:31 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+http.createServer(function(req,res){
+
+res.write("On the way to fullstack engineering!!!");
+res.end();
+
+
+}
+).listen(3000);
+
+console.log("Server started at port 3000");
